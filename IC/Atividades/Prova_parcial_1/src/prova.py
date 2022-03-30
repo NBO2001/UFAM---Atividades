c99339b883ee27be3492fd2c81b885aa1a1eee8b
Natanael Bezerra de Oliveira <natanbezerr63@gmail.com>
2022-03-30 18:44:07 -0400

    Aplicando blue

--- IC/Atividades/Prova_parcial_1/src/prova.py
+++ IC/Atividades/Prova_parcial_1/src/prova.py
@@ -2,44 +2,58 @@
 # Releve o péssimo nome das funções e variavéis
 
 # Questão 1
-def pontos(p1,p2): 
+def pontos(p1, p2):
 
     a, b = p1
     c, d = p2
 
-    if ( a + c)%5 == 0: return a+c
-    if ( a + d)%5 == 0: return a+d
-    if ( b + c)%5 == 0: return b+c
-    if ( b + d)%5 == 0: return b+d
-    else: return 0
+    if (a + c) % 5 == 0:
+        return a + c
+    if (a + d) % 5 == 0:
+        return a + d
+    if (b + c) % 5 == 0:
+        return b + c
+    if (b + d) % 5 == 0:
+        return b + d
+    else:
+        return 0
+
 
 # Questão 2
-def retira_y(x, xs): return [y for y in xs if y != x]
+def retira_y(x, xs):
+    return [y for y in xs if y != x]
+
 
 def retira(ks, xs):
 
     for k in ks:
         xs = retira_y(k, xs)
-    
+
     return xs
 
+
 # Questão 3
-def divs(x): return [ div_num for div_num in range(1, x+1) if (x%div_num) == 0]
+def divs(x):
+    return [div_num for div_num in range(1, x + 1) if (x % div_num) == 0]
+
 
 # Questão 4
-def tuples_equals(tups): return [(a, b) for a, b in tups if a == b]
+def tuples_equals(tups):
+    return [(a, b) for a, b in tups if a == b]
+
 
 def maior_number(list_tupes):
     maiorA, maiorB = list_tupes[0]
     maior = (maiorA, maiorB)
 
     for c, d in list_tupes:
-        
+
         if maiorA <= c:
             maior = (c, d)
-    
+
     return maior
+
 
 def nomax(m):
     maiorA, maiorB = maior_number(tuples_equals(m))
-    return [ (a,b) for a,b in m if maiorA !=a and maiorB != b]
+    return [(a, b) for a, b in m if maiorA != a and maiorB != b]
