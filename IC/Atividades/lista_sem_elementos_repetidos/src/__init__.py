167a828399a5276994fce4e15a13949423bae198
Natanael Bezerra de Oliveira <natanbezerr63@gmail.com>
2022-04-27 00:02:08 -0400

    Nova versão norep

--- IC/Atividades/lista_sem_elementos_repetidos/src/__init__.py
+++ IC/Atividades/lista_sem_elementos_repetidos/src/__init__.py
@@ -1,17 +1,14 @@
-
 def norep(xs):
-    
-    lista_ordenada = order_list(xs)
-    list_final = []
 
-    for element in lista_ordenada:
-        if not contains_in(element,list_final):
-            list_final.append(element)
+    if len(xs) == 0:
+        return []
 
-    return list_final
+    else:
+        return [head(xs)] + norep(tira(head(xs), tail(xs)))
 
 
 # AUxiliares
+
 
 def head(lista_itens):
     if len(lista_itens) == 0:
@@ -20,44 +17,18 @@ def head(lista_itens):
         return lista_itens[0]
 
 
+def tira(k, xs):
+
+    if len(xs) == 0:
+        return []
+    elif head(xs) != k:
+        return [head(xs)] + tira(k, tail(xs))
+    else:
+        return tira(k, tail(xs))
+
+
 def tail(lista_itens, position=1):
     if len(lista_itens) == 0:
         return []
     else:
         return lista_itens[position:]
-
-def contains_in(value, list_values):
-
-    if len(list_values) == 0:
-
-        return False
-
-    elif value == head(list_values):
-
-        return True
-
-    else:
-
-        return contains_in(value, tail(list_values))
-
-
-def append_element_ord(number, lista_of_numbers):
-
-    if len(lista_of_numbers) == 0:
-        return [number]
-    elif number <= head(lista_of_numbers):
-
-        return [number] + lista_of_numbers
-    else:
-        return [head(lista_of_numbers)] + append_element_ord(
-            number, tail(lista_of_numbers)
-        )
-
-
-def order_list(lista_itns):
-
-    if len(lista_itns) == 0:
-        return []
-
-    return append_element_ord(head(lista_itns), order_list(tail(lista_itns)))
-
--- IC/Atividades/lista_sem_elementos_repetidos/tests/test_norep.py
+++ IC/Atividades/lista_sem_elementos_repetidos/tests/test_norep.py
@@ -1,4 +1,5 @@
 from src import norep
 
+
 def test_norep_lista_sem_repetidos():
-    assert norep([2,5,8,5,5,2]) == [2,5,8]
+    assert norep([2, 5, 8, 5, 5, 2]) == [2, 5, 8]
