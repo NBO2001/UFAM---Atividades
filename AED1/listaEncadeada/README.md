2b4cbc65caaaa78aa5d2c3abb4d202fb1814ea32
NatanaelB <natanaelbezerra36@gmail.com>
2022-07-24 19:33:23 -0400

    Creante structs, funcs: list, node,inset, showData and showList

---
+++ AED1/listaEncadeada/README.md
@@ -0,0 +1,16 @@
+# Estruta de dados lista
+
+A seguir, propriedades vistas em Knuth(1968, p. 235) e Aho, Hopcroft e Ullman (1983, p. 38-39)
+
+Propriedades de uma lista:
+
+
+1. Criar um lista vazia.
+2. Inserir um novo item imediatamente apos o i-esimo item.
+3. Retirar o i-esimo item.
+4. Localizar o i-esimo item para examinar e/ou alterar o conteudo de seus componentes.
+5. Combinar duas ou mais listas lineares em uma.
+6. Partir uma lista linear em duas ou mais listas.
+7. Fazer copia de uma lista linear.
+8. Ordenar os itens da lista em ordem ascendente ou descendente, de acordo com algun dos seus componentes.
+9. Pesquisar as ocorrencais de uma valor particular em algum componente. 
--- AED1/listaEncadeada/main.c
+++ AED1/listaEncadeada/main.c
@@ -1,10 +1,103 @@
 #include <stdio.h>
-/*
-criar um lista vazia
+#include <stdlib.h>
 
-*/
+typedef struct typeData{
+    
+    int d;
+
+}typeData;
+
+typedef struct typeNode{
+    
+    typeData data;
+    struct typeNode * next;
+
+}typeNode;
+
+typedef struct typeList{
+
+    typeNode * first;
+
+}typeList;
+
+// Create list
+typeList * list(){
+
+    typeList * lst;
+
+    lst = (typeList * ) malloc(sizeof(typeList));
+
+    lst->first = NULL;
+
+    return lst;
+
+}
+
+// Create node
+typeNode * node(typeData *dt){
+
+    typeNode * nod;
+
+    nod = (typeNode * ) malloc(sizeof(typeNode));
+
+    nod->data = *dt;
+
+    nod->next = NULL;
+
+    return nod;
+
+}
+
+// Insert in list
+void insert(typeList * lst, typeData dat){
+
+    typeNode * nd;
+    typeNode * aux;
+
+    nd = node(&dat);
+
+    aux = lst->first;
+    nd->next = aux;
+    lst->first = nd;
+
+}
+
+// Show only data
+void showData(typeData *dat){
+
+    printf("%d\n", dat->d);
+
+}
+
+
+// Show list
+void showList(typeList * lst){
+    
+    typeNode * aux;
+
+    aux = lst->first;
+
+    while(aux){
+
+        showData(&aux->data);
+
+        aux = aux->next;
+    }
+
+}
+
 int main(){
 
-    printf("Hello word\n");
+    typeList * l1;
+    typeData n1;
+
+    l1 = list();
+
+    for(int i=0; i < 5; i++){
+        n1.d = i;
+        insert(l1,n1);
+    }
+
+    showList(l1);
 
 }
